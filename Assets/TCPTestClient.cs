89dc2581a522c2138bc08ed32e4fef6678103c1d
Anthony-Amgad <19p9880@eng.asu.edu.eg>
2023-04-28 19:56:14 +0300

    Transfering positions between 2 players

--- Assets/TCPTestClient.cs
+++ Assets/TCPTestClient.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
@@ -14,11 +12,14 @@ public class TCPTestClient : MonoBehaviour {
 
 	public Transform Player;
 
+	public Transform Opp;
+	private String Oppos = "(0.0,11.32,0)";
+
 	float timeToGo;
-	const float timeOffset = 0.1f;
+	const float timeOffset = 0.02f;
 
 	const int PORT_NO = 50000;
-    const string SERVER_IP = "localhost"; //replace local host with your device ip	
+    const string SERVER_IP = "192.168.56.1"; //replace local host with your device ip	
 	
 	void Start () {
 		timeToGo = Time.fixedTime + timeOffset;
@@ -29,19 +30,38 @@ public class TCPTestClient : MonoBehaviour {
 		catch (Exception e) { 			
 			Debug.Log("On client connect exception " + e); 		
 		}
+		Thread thread = new Thread(new ThreadStart(Listen));
+        thread.Start();
 		    
 	}  	
-	// Update is called once per frame
-	void FixedUpdate() {
-		if (Time.fixedTime >= timeToGo) {
-			byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(Player.position.ToString());
-			nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+	
+	//Listening thread for any other players
+	void Listen(){
+		while(true){
 			byte[] bytesToRead = new byte[socketConnection.ReceiveBufferSize];
 			int bytesRead = nwStream.Read(bytesToRead, 0, socketConnection.ReceiveBufferSize);
 			Debug.Log("Received : " + Encoding.ASCII.GetString(bytesToRead, 0, bytesRead));
-			timeToGo = Time.fixedTime + timeOffset;
-		}
+			Oppos = Encoding.ASCII.GetString(bytesToRead, 0, bytesRead);
+		}	
 	}
+	
+	//sending position every 0.02 second **NEEDS LOWERING MOST RROB
+	void FixedUpdate() {
+		try{
+			if (Time.fixedTime >= timeToGo) {
+			byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(Player.position.ToString());
+			nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+			timeToGo = Time.fixedTime + timeOffset;
+			}
+		}catch(Exception e)
+		{
+			Debug.Log(e.Message);
+		}
+
+		Opp.position = Vector3FromString(Oppos);
+	}
+		
+	
 	
 	private void OnApplicationQuit() {
 		try
@@ -53,5 +73,19 @@ public class TCPTestClient : MonoBehaviour {
 			Debug.Log(e.Message);
 		}
 	}
-	
+
+	Vector3 Vector3FromString(String Vector3string) {
+		String outString;
+		Vector3 outVector;
+		String[] splitString = {};
+		outString = Vector3string.Substring(1, Vector3string.Length -2);
+		splitString = outString.Split(","[0]);
+		outVector.x = float.Parse(splitString[0]);
+   		outVector.y = float.Parse(splitString[1]);
+   		outVector.z = float.Parse(splitString[2]);
+   
+		return outVector;
+	}
+
 }
+
--- Server.py
+++ Server.py
@@ -1,4 +1,24 @@
 import socket
+import _thread
+
+addrs = []
+positions = ['x','x','x','x']
+count = 0
+
+def on_new_client(clientsocket,addr,num):
+    while True:
+        msg = clientsocket.recv(1024).decode('utf-8')
+        #do some checks and if msg == someWeirdSignal: break:
+        print(addr, ' >> ', msg)
+        positions[num] = msg
+        for i in range(0,4):
+            if (i != num) and (positions[i] != 'x'):
+                msg = str(positions[i])
+                #Maybe some code to compute the last digit of PI, play game or anything else can go here and when you are done.
+                clientsocket.send(msg.encode('utf-8'))
+    clientsocket.close()
+
+
 s = socket.socket()
 host = socket.gethostname()
 port=50000
@@ -7,13 +27,15 @@ print('Server started!')
 print('Waiting for clients...')
 
 s.bind((host, port))        # Bind to the port
-s.listen()                 # Now wait for client connection.
-c, addr = s.accept()     # Establish connection with client.
-print('Got connection from', addr)
+s.listen(4)                 # Now wait for client connection.                            
+
 while True:
-    try:
-        msg = c.recv(1024).decode('utf-8')
-        print(addr, ' >> ', msg)
-        c.send(msg.encode('utf-8'))
-    except:
-        c.close()  
+    c, addr = s.accept()    # Establish connection with client.
+    print('Got connection from', addr)
+    addrs.append(addr)
+    _thread.start_new_thread(on_new_client,(c,addr,count))
+    count+=1
+    #Note it's (addr,) not (addr) because second parameter is a tuple
+    #Edit: (c,addr)
+    #that's how you pass arguments to functions when creating new threads using thread module.
+s.close() 
